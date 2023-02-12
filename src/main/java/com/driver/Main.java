91aa5be8a9d40137b932219f8ca1ad24bcd74eb6
sunilkumar sajjan <sunilkumarsajjan777@gmail.com>
2023-02-12 11:53:07 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] args)
+  {    RWOnly a=new RWOnly();
+    a.setName("sunil");
+    System.out.print(a.getName());
+}
 }
